e76cdec24fff0ee5bfd27239a407a7eb0694f629
William Funkenbusch <williamfunkenbusch@gmail.com>
2018-09-19 11:21:07 -0400

    Added randomization to RGK for random walk. Added a unit test.

--- Langevin/Langevin.py
+++ Langevin/Langevin.py
@@ -2,14 +2,14 @@
 
 import numpy as np
 
-def RGK(fun, t, y0):
+def RGK(fun, t, y0, rand = None):
     '''
     Takes a function, time range, and initial conditions, and numerically integratesthe function according to the Runge-Kutta Method.
 
     Arguments:
     fun (function):
     
-    The ODE('s) to be integrated. Should take the independent variable (t) and the dependent variable (y) as arguments, in that order. Should return the derivative ofy with respect to t.
+    The ODE('s) to be integrated. Should take the independent variable (t) and the dependent variable (y) as arguments, in that order. Should return the derivative of y with respect to t.
 
     For multiple ordinary differential equations, the dependent variable input should be a list of each variable. The output should return each derivative in the same order that the variables were input.
 
@@ -22,6 +22,15 @@ def RGK(fun, t, y0):
     y0 (list or array):
 
     The initial values of each variable to be integrated. Must be in the same order as the output of fun.
+
+    rand (list or array):
+
+    A random value to be added to a variable. The first input is the variable to be added to (indexed 0, 1,...). The second input is the mean of the distribution, the third input is the standard deviation of the distribution. The value is pulled from a normal distribution. The value will be multiplied by the change in t at that time step. This option can be opted out of by setting rand = None, which is the default.
+
+    Returns:
+    F (matrix): 
+    
+    The solution to the ODE's stacked vertically. The order of stacking is the same as the order of the output of fun.
     '''
     F = np.zeros((len(y0), len(t)))
     F[:, 0] = y0
@@ -39,6 +48,8 @@ def RGK(fun, t, y0):
         k4 = h*fun(tn + h, yn + k3)
 
         F[:, i + 1] = yn + 1/6*(k1 + 2*k2 + 2*k3 + k4)
+        if rand != None:
+            F[rand[0], i + 1] += np.random.normal(rand[1], rand[2])*h
 
     return F
         
--- tests/test_Langevin.py
+++ tests/test_Langevin.py
@@ -7,10 +7,10 @@ from Langevin import Langevin
 from Langevin.Langevin import *
 
 class RGK_unit_tests(unittest.TestCase):
-    def test(self):
+    def test_increment(self):
         t = [0, 1]
         x0 = 0
-        x = RGK(lambda x, t: t*x**2, t, [x0])
+        x = RGK(lambda x, t: t*x**2, t, [x0], rand = None)
 
         h = t[1] - t[0]
         k1 = h*t[0]*x0**2
@@ -20,6 +20,14 @@ class RGK_unit_tests(unittest.TestCase):
 
         xf = x0 + 1/6*(k1 + 2*k2 + 2*k3 + k4)
         self.assertEqual(x[0, 1], xf)
+
+    def test_rand(self):
+        np.random.seed(1234)
+        t = np.linspace(0, 1, 100)
+        x0 = 0
+        val1 = RGK(lambda x, t: t + x, t, [x0], rand = None)
+        val2 = RGK(lambda x, t: t + x, t, [x0], rand = [0, 0, 1])
+        self.assertNotEqual(val1[0, -1], val2[0, -1])
 
 if __name__ == "__main__":
 	unittest.main()

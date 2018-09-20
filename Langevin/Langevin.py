3a7d5ac49de1e42982a3efd734f50a45d18e99db
William Funkenbusch <williamfunkenbusch@gmail.com>
2018-09-19 21:01:45 -0400

    Added Brownian motion ODE function with unit tests and function  to save output.

--- Langevin/Langevin.py
+++ Langevin/Langevin.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def RGK(fun, t, y0, rand = None):
+def RGK(fun, t, y0, vals, rand = None):
     '''
     Takes a function, time range, and initial conditions, and numerically integratesthe function according to the Runge-Kutta Method.
 
@@ -23,9 +23,13 @@ def RGK(fun, t, y0, rand = None):
 
     The initial values of each variable to be integrated. Must be in the same order as the output of fun.
 
+    vals (list or array):
+
+    The problem parameters which fun must take in addition to t and y. Should be in the same order that they are required in the function.
+
     rand (list or array):
 
-    A random value to be added to a variable. The first input is the variable to be added to (indexed 0, 1,...). The second input is the mean of the distribution, the third input is the standard deviation of the distribution. The value is pulled from a normal distribution. The value will be multiplied by the change in t at that time step. This option can be opted out of by setting rand = None, which is the default.
+    A random value to be added to a variable. The first input is the variable to be added to (indexed 0, 1,...). The second input is the mean of the distribution, the third input is the standard deviation of the distribution. The value is pulled from a normal distribution. The value will be multiplied by the change in t at that time step and divided by the mass, m (vals[0]). This option can be opted out of by setting rand = None, which is the default.
 
     Returns:
     F (matrix): 
@@ -42,14 +46,71 @@ def RGK(fun, t, y0, rand = None):
         tn = t[i + 1]
         h = t[i + 1] - t[i]
 
-        k1 = h*fun(tn, yn)
-        k2 = h*fun(tn + h/2, yn + k1/2)
-        k3 = h*fun(tn + h/2, yn + k2/2)
-        k4 = h*fun(tn + h, yn + k3)
+        k1 = h*fun(tn, yn, vals)
+        k2 = h*fun(tn + h/2, yn + k1/2, vals)
+        k3 = h*fun(tn + h/2, yn + k2/2, vals)
+        k4 = h*fun(tn + h, yn + k3, vals)
 
         F[:, i + 1] = yn + 1/6*(k1 + 2*k2 + 2*k3 + k4)
         if rand != None:
-            F[rand[0], i + 1] += np.random.normal(rand[1], rand[2])*h
+            F[rand[0], i + 1] += np.random.normal(rand[1], rand[2])*h/vals[0]
 
     return F
-        
+
+def ODE(t, x0, vals):
+    '''
+    Takes a time, position, and velocity, and returns the velocity and acceleration for 1D Brownian motion with no random force.
+
+    Arguments:
+
+    t (float):
+
+    The independent variable.
+
+    x0 (list or array):
+
+    The dependent variables. The first input should be the position, and the second input should be the velocity.
+
+    vals (list or array):
+
+    The Brownian motion parameters. The first input should be mass, the second input should be the damping coefficient (gamma) (all in SI units).
+
+    Returns:
+
+    dxdt, dvdt (list):
+
+    The velocity and acceleration at the specified time, position, and velocity.
+    '''
+    x = x0[0]
+    v = x0[1]
+    m = vals[0]
+    gamma = vals[1]
+    
+    dxdt = v
+    dvdt = -gamma*v/m
+
+    return dxdt, dvdt
+
+def Langevin(FileName, t_t, dt, init_pos, init_vel, m, gamma, T, Lambda = 1):
+    t = np.linspace(0, t_t, int(t_t/dt))
+    rand = [1, 0, np.sqrt(2*1.38064852*10**(-23)*T*Lambda*dt] #Adds to velocity, centered at 0, standard deviation of sqrt(2k_B*T*lambda*(t - t')
+    vals = [m, gamma]
+    x0 = [init_pos, init_vel]
+
+    ans = RGK(ODE, t, x0, vals, rand = rand)
+    x = ans[0, :]
+    v = ans[1, :]
+
+    print('The final position of the particle was {:.3} m.' .format(x[-1]))
+    print('The final velocity of the particle was {:.3} m/s.' .format(v[-1]))
+
+    lines = ['index, t, x, v']
+
+    for i in range(len(x)):
+        lines.append(str(i) + ' ' + str(t[i]) + ' ' + str(x[i]) + ' ' + str(v[i]))
+
+    F = open(FileName + '.txt', 'w')
+    for i in range(len(lines)):
+        F.write(lines[i])
+    F.close()
+
--- tests/test_Langevin.py
+++ tests/test_Langevin.py
@@ -10,7 +10,8 @@ class RGK_unit_tests(unittest.TestCase):
     def test_increment(self):
         t = [0, 1]
         x0 = 0
-        x = RGK(lambda x, t: t*x**2, t, [x0], rand = None)
+        vals = [1, 1]
+        x = RGK(lambda x, t, vals: t*x**2, t, [x0], vals, rand = None)
 
         h = t[1] - t[0]
         k1 = h*t[0]*x0**2
@@ -25,9 +26,28 @@ class RGK_unit_tests(unittest.TestCase):
         np.random.seed(1234)
         t = np.linspace(0, 1, 100)
         x0 = 0
-        val1 = RGK(lambda x, t: t + x, t, [x0], rand = None)
-        val2 = RGK(lambda x, t: t + x, t, [x0], rand = [0, 0, 1])
+        vals = [1, 1, 1]
+        val1 = RGK(lambda x, t, vals: t + x, t, [x0], vals, rand = None)
+        val2 = RGK(lambda x, t, vals: t + x, t, [x0], vals, rand = [0, 0, 1])
         self.assertNotEqual(val1[0, -1], val2[0, -1])
+
+class ODE_unit_tests(unittest.TestCase):
+    def test_position(self):
+
+        dxdt, dvdt = ODE(0, [1, 1], [1, 1])
+        self.assertEqual(dxdt, 1)
+
+    def test_velocity(self):
+        dxdt, dvdt = ODE(0, [1, 1], [1, 1])
+        self.assertEqual(dvdt, -1)
+
+    def test_stationary(self):
+        dxdt, dvdt = ODE(0, [1, 0], [1, 1])
+        self.assertEqual(dxdt, 0)
+
+    def test_no_drag(self):
+        dxdt, dvdt = ODE(0, [1, 1], [1, 0])
+        self.assertEqual(dvdt, 0)
 
 if __name__ == "__main__":
 	unittest.main()
